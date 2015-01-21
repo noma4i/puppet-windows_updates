0a4d30003931b8d4cd126f7b0ea8c0ac999a80a6
Alexander Tsirel <noma4i@gmail.com>
2015-01-21 16:15:16 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# windows_updates
+
+Puppet module to install selected windows updates or mask of updated etc.
+
+#### Available options
+
+#### How to use
+
+
+```puppet
+  }
+````
---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+class windows_updates (
+  $ensure   = 'enabled',
+  $kb = undef,
+){
+  case $ensure {
+    'enabled', 'present': {
+      exec { 'Install PSModule to manage Windows Updates':
+        command  => template('windows_updates/install_psupdates.ps1.erb'),
+        provider => 'powershell',
+        onlyif   => 'if ((Test-Path -Path "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\PSWindowsUpdate") -eq $True) { exit 0 } else { exit 1 }',
+        timeout  => 600
+      }
+      # ->
+      # exec { "Install ${kb}":
+      #   command  => template('windows_updates/install_kb.ps1.erb'),
+      #   provider => 'powershell',
+      #   timeout  => 600
+      # }
+    }
+    default: {
+      fail('Invalid ensure option!\n')
+    }
+  }
+}
