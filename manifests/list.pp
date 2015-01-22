8af5b55c5d9ee6b56c9634a69b78fe335d4f5534
Alexander Tsirel <noma4i@gmail.com>
2015-01-22 16:31:21 +1100

    puppet-lint --fix

--- manifests/list.pp
+++ manifests/list.pp
@@ -1,13 +1,13 @@
 define windows_updates::list (
   $ensure   = 'enabled',
   $name = undef,
-  $dry_run = 'false'
+  $dry_run = false
 ){
   require windows_updates
 
   case $ensure {
     'enabled', 'present': {
-      if $dry_run == 'false'{
+      if $dry_run == false{
         exec { "Install Updates By Name ${name}":
           command  => template('windows_updates/install_by_title.ps1.erb'),
           provider => 'powershell',
