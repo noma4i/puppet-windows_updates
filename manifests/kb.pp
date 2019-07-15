3bbb46682ae9423a238551c289ac1a69d7c77dda
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-07-15 11:58:54 +0200

    Add special handling of KB890830
    Ensure Malicious Software Removal Tool can run every month

--- manifests/kb.pp
+++ manifests/kb.pp
@@ -7,13 +7,28 @@ define windows_updates::kb (
 
   case $ensure {
     'enabled', 'present': {
-      exec { "Install ${kb}":
-        command   => template('windows_updates/install_kb.ps1.erb'),
-        creates   => "C:\\ProgramData\\InstalledUpdates\\${kb}.flg",
-        provider  => 'powershell',
-        timeout   => 0,
-        logoutput => true,
-        schedule  => $maintwindow
+      case $kb {
+        'KB890830', 'kb890830': {
+          #Don't skip this recurring monthly update (Malicious Software Removal Tool)
+          exec { "Install ${kb}":
+            command   => template('windows_updates/install_kb.ps1.erb'),
+            provider  => 'powershell',
+            timeout   => 0,
+            logoutput => true,
+            schedule  => $maintwindow
+          }
+        }
+        default: {
+          #Run update if it hasn't successfully run before
+          exec { "Install ${kb}":
+            command   => template('windows_updates/install_kb.ps1.erb'),
+            creates   => "C:\\ProgramData\\InstalledUpdates\\${kb}.flg",
+            provider  => 'powershell',
+            timeout   => 0,
+            logoutput => true,
+            schedule  => $maintwindow
+          }
+        }
       }
     }
     default: {
