0d7c5c38c0f8702ac52b8014086813ceb71c88f4
Alexander Tsirel <noma4i@gmail.com>
2019-08-25 13:36:57 +1000

    fix rubocop

--- lib/facter/windows_updates_reboot_needed.rb
+++ lib/facter/windows_updates_reboot_needed.rb
@@ -1,15 +1,15 @@
 Facter.add('updates_reboot_needed') do
-    confine kernel: 'windows'
-    setcode do
-      sysroot = ENV['SystemRoot']
-      powershell = "#{sysroot}\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
-      # get the script path relative to facter Ruby program
-      script_file = File.join(
-        File.expand_path(File.dirname(__FILE__)),
-        '..',
-        'windows_updates',
-        'checkreboot.ps1',
-      )
-      Facter::Util::Resolution.exec("#{powershell} -ExecutionPolicy Unrestricted -File #{script_file}").to_s == "true"
-    end
+  confine kernel: 'windows'
+  setcode do
+    sysroot = ENV['SystemRoot']
+    powershell = "#{sysroot}\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
+    # get the script path relative to facter Ruby program
+    script_file = File.join(
+      File.expand_path(File.dirname(__FILE__)),
+      '..',
+      'windows_updates',
+      'checkreboot.ps1',
+    )
+    Facter::Util::Resolution.exec("#{powershell} -ExecutionPolicy Unrestricted -File #{script_file}").to_s == 'true'
   end
+end
